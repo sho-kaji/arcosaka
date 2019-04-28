7f26ece03d47de1ba7d5c3961a9b42a624a5fcd1
a amano <akno@live.jp>
2019-04-28 15:36:14 +0900

    ROSインストールスクリプト修正

--- 2019/ros_install.sh
+++ 2019/ros_install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 &&
 sudo apt update &&
@@ -9,21 +11,21 @@ source ~/.bashrc &&
 sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential &&
 
 prgname='kakasi'
-cd ~/${prgname}/src &&
+cd ~/arcosaka/2019/${prgname}/src &&
 catkin_init_workspace &&
-cd ~/${prgname} &&
+cd ~/arcosaka/${prgname} &&
 catkin_make &&
 
-prgname='kusakari'
-cd ~/${prgname}/src &&
+prgname='kusakari' &&
+cd ~/arcosaka/2019/${prgname}/src &&
 catkin_init_workspace &&
-cd ~/${prgname} &&
+cd ~/arcosaka/${prgname} &&
 catkin_make &&
 
-prgname='mekaki'
-cd ~/${prgname}/src &&
+prgname='mekaki' &&
+cd ~/arcosaka/2019/${prgname}/src &&
 catkin_init_workspace &&
-cd ~/${prgname} &&
+cd ~/arcosaka/${prgname} &&
 catkin_make &&
 
-echo "OK" 
+echo "OK"
