178b5af6c99d78bb1795084b39dd3093595c900d
a amano <akno@live.jp>
2019-04-28 11:40:49 +0900

    インストールスクリプト追加

---
+++ 2019/ros_install.sh
@@ -0,0 +1,29 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 &&
+sudo apt update &&
+sudo apt install -y ros-melodic-desktop &&
+sudo rosdep init &&
+rosdep update &&
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc &&
+source ~/.bashrc &&
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential &&
+
+prgname='kakasi'
+cd ~/${prgname}/src &&
+catkin_init_workspace &&
+cd ~/${prgname} &&
+catkin_make &&
+
+prgname='kusakari'
+cd ~/${prgname}/src &&
+catkin_init_workspace &&
+cd ~/${prgname} &&
+catkin_make &&
+
+prgname='mekaki'
+cd ~/${prgname}/src &&
+catkin_init_workspace &&
+cd ~/${prgname} &&
+catkin_make &&
+
+echo "OK" 
