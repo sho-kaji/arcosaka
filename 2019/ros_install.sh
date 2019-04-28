2b693d31577d4c4d654ed6f92b115b5429872028
osakarp <arcosaka@outlook.jp>
2019-04-28 22:41:23 +0900

    ros-melodic-desktop-full にて作成

--- 2019/ros_install.sh
+++ 2019/ros_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 &&
 sudo apt update &&
-sudo apt install -y ros-melodic-desktop &&
+sudo apt install -y ros-melodic-desktop-full &&
 sudo rosdep init &&
 rosdep update &&
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc &&
@@ -12,6 +12,7 @@ sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool 
 
 for prgname in kakasi kusakari mekaki
 do
+    echo "[${prgname}]を設定開始" &&
     mkdir -p ~/arcosaka/2019/${prgname}/src &&
     cd ~/arcosaka/2019/${prgname}/src &&
     catkin_init_workspace &&
@@ -19,4 +20,4 @@ do
     catkin_make
 done &&
 
-echo "OK"
+echo "OK!"
