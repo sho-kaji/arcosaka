f7034306fc119f3040f91dc4892fe941630c1319
a amano <akno@live.jp>
2019-04-28 22:19:37 +0900

    ROS melodic インストールスクリプト修正

--- 2019/ros_install.sh
+++ 2019/ros_install.sh
@@ -10,22 +10,13 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc &&
 source ~/.bashrc &&
 sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential &&
 
-prgname='kakasi'
-cd ~/arcosaka/2019/${prgname}/src &&
-catkin_init_workspace &&
-cd ~/arcosaka/${prgname} &&
-catkin_make &&
-
-prgname='kusakari' &&
-cd ~/arcosaka/2019/${prgname}/src &&
-catkin_init_workspace &&
-cd ~/arcosaka/${prgname} &&
-catkin_make &&
-
-prgname='mekaki' &&
-cd ~/arcosaka/2019/${prgname}/src &&
-catkin_init_workspace &&
-cd ~/arcosaka/${prgname} &&
-catkin_make &&
+for prgname in kakasi kusakari mekaki
+do
+    mkdir -p ~/arcosaka/2019/${prgname}/src &&
+    cd ~/arcosaka/2019/${prgname}/src &&
+    catkin_init_workspace &&
+    cd ~/arcosaka/2019/${prgname} &&
+    catkin_make
+done &&
 
 echo "OK"
