fa55254dfabf2ec40d3a35b68116756ce785f576
osakarp <arcosaka@outlook.jp>
2019-04-30 22:16:04 +0900

    test_webrtcを修正

--- 2019/ros_set.sh
+++ 2019/ros_set.sh
@@ -9,7 +9,7 @@ source /opt/ros/melodic/setup.bash
 source ~/arcosaka/2019/${ROS_PRGNAME}/devel/setup.bash
 export ROS_HOSTNAME=osakarp.local
 export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
-alias cw=cd ~/arcosaka/2019/${ROS_PRGNAME}
-alias cs=cd ~/arcosaka/2019/${ROS_PRGNAME}/src
-alias cm=cd ~/arcosaka/2019/${ROS_PRGNAME} && catkin_make
-cd ~/arcosaka/2019/${ROS_PRGNAME}/src
+alias "cw=cd ~/arcosaka/2019/${ROS_PRGNAME}/"
+alias "cs=cd ~/arcosaka/2019/${ROS_PRGNAME}/src/"
+alias "cm=cd ~/arcosaka/2019/${ROS_PRGNAME}/ && catkin_make"
+cd ~/arcosaka/2019/${ROS_PRGNAME}/src/
