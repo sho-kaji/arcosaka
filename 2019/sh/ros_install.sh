a284d0ea5a8e4764f070515f73c7640001a3a2ee
a amano <akno@live.jp>
2019-04-30 09:17:53 +0900

    test_webrtcのテスト

--- 2019/sh/ros_install.sh
+++ 2019/sh/ros_install.sh
@@ -3,10 +3,11 @@
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 &&
 sudo apt update &&
-sudo apt install -y ros-melodic-desktop-full &&
+sudo apt -y install ros-melodic-desktop-full &&
 sudo rosdep init &&
 rosdep update &&
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc &&
 source ~/.bashrc &&
-sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential &&
+sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential &&
+sudo apt -y install ros-melodic-usb-cam &&
 ./ros_setup.sh
