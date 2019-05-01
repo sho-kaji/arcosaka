7fb231782fc73dff998a9d6679cf5df6e4005399
osakarp <arcosaka@outlook.jp>
2019-05-01 13:37:28 +0900

    test_webrtcを修正2

--- 2019/ros_run.sh
+++ 2019/ros_run.sh
@@ -8,6 +8,7 @@ echo "ROS_PRGNAME=「${ROS_PRGNAME}」に設定"
 if ! pgrep roslaunch > /dev/null; then
     source /opt/ros/melodic/setup.bash
     source ~/arcosaka/2019/${ROS_PRGNAME}/devel/setup.bash
+    export ROS_SRCDIR="~/arcosaka/2019/${ROS_PRGNAME}/src"
     export ROS_HOSTNAME=osakarp.local
     export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
     alias "cw=cd ~/arcosaka/2019/${ROS_PRGNAME}/"
