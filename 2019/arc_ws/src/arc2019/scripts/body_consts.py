8b3173fb2f807b81096b77655c9d8bc9f4ff5400
a amano <akno@live.jp>
2019-09-16 22:30:16 +0900

    改行コード変更

--- 2019/arc_ws/src/arc2019/scripts/body_consts.py
+++ 2019/arc_ws/src/arc2019/scripts/body_consts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 body.py 定数ファイル
--- 2019/arc_ws/src/arc2019/scripts/hand_consts.py
+++ 2019/arc_ws/src/arc2019/scripts/hand_consts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 hand.py 定数ファイル
