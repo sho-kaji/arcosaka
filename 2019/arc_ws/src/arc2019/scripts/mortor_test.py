c6dc8cbbbd52742514cebc99925fda5b937240c9
Akinao Amano <akno@live.jp>
2019-09-22 11:58:17 +0900

    DCモーター用テスト

--- 2019/arc_ws/src/arc2019/scripts/mortor_test.py
+++ 2019/arc_ws/src/arc2019/scripts/mortor_test.py
@@ -6,8 +6,32 @@
 """
 
 import mortor
+from mortor_consts import \
+    DCROTATE
 
 def main():
+    mc = mortor.MortorClass()
+    while True:
+        try:
+            val = input('number:')
+            int_tmp = int(val)
+            if int_tmp == 0:
+                mc.move_dc(16,20,DCROTATE.STOP)
+            elif int_tmp == 1:
+                mc.move_dc(16,20,DCROTATE.CW)
+            elif int_tmp == -1:
+                mc.move_dc(16,20,DCROTATE.CCW)
+            else:
+                break
+
+        except KeyboardInterrupt:
+            print("Ctrl+Cで停止しました")
+            break
+        except :
+            pass
+    mc.endfnc()
+
+def main_servo():
     mc = mortor.MortorClass()
     while True:
         try:
@@ -28,7 +52,6 @@ def main():
         except :
             pass
     mc.endfnc()
-
 
 if __name__ == '__main__':
     main()
