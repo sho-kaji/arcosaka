b6fd8479c37cbc9f1db380ffd2689aaa006f16e3
Akinao Amano <akno@live.jp>
2019-10-05 16:46:53 +0900

    360サーボのテスト用

--- 2019/arc_ws/src/arc2019/scripts/ina226.py
+++ 2019/arc_ws/src/arc2019/scripts/ina226.py
@@ -5,10 +5,18 @@
 電流電圧測定IC
 """
 
+from subprocess import call
 import time
 import smbus
 
-ADDR_INA226 = 0x40 #I2Cアドレス(固定)
+
+I2C_INA226 = 0x40 #I2Cアドレス(固定)
+ADDR_V = 0x02
+ADDR_I = 0x04
+ADDR_R = 0x05
+
+BATT_LOW = 6.2
+BATT_ERR = 10
 
 class Ina226(object):
     """
@@ -16,13 +24,20 @@ class Ina226(object):
     """
     def __init__(self):
         self.i2c = smbus.SMBus(1)
-        self.i2c.write_word_data(ADDR_INA226, 0x05, 0x14)
+        self.i2c.write_word_data(I2C_INA226, ADDR_R, 0x14)
+
+        self.is_battlow = False
+        self.cnt_battlow = 0
+        self.v_ave = 0
+        self.i_ave = 0
+        self.read_v()
+        self.read_i()
 
     def read_v(self):
         """
         電圧読み取り
         """
-        word = self.i2c.read_word_data(ADDR_INA226, 0x02) & 0xFFFF
+        word = self.i2c.read_word_data(I2C_INA226, ADDR_V) & 0xFFFF
         result = ((word << 8) & 0xFF00) + (word >> 8)
         volt = result * 1.25 / 1000
         return volt
@@ -31,7 +46,7 @@ class Ina226(object):
         """
         電流読み取り
         """
-        word = self.i2c.read_word_data(ADDR_INA226, 0x04) & 0xFFFF
+        word = self.i2c.read_word_data(I2C_INA226, ADDR_I) & 0xFFFF
         curr = ((word << 8) & 0xFF00) + (word >> 8)
         return curr
 
@@ -39,11 +54,24 @@ class Ina226(object):
         """
         電流電圧読み取りループ
         """
-        self.read_v()
-        self.read_i()
         while True:
-            print str(self.read_v()) + "[V]"
-            print str(self.read_i()) + "[mA]"
+            v_now = self.read_v()
+            i_now = self.read_i() / 1000.0
+            self.v_ave = (self.v_ave + v_now) / 2.0
+            self.i_ave = (self.i_ave + i_now) / 2.0
+            print str(v_now) + "[V]\t" + str(self.v_ave) + "[V]\t" + \
+                str(i_now) + "[A]\t" + str(self.i_ave) + "[A]"
+            if self.v_ave < BATT_LOW:
+                self.cnt_battlow += 1
+            else:
+                self.cnt_battlow = 0
+
+            if self.cnt_battlow > BATT_ERR:
+                print "シャットダウンします"
+                call('sudo shutdown -h 1', shell=True)
+            else:
+                pass
+
             time.sleep(1)
 
 
