8eee7a33d683bca7f0124b29a5146bf7ff72a2e6
Akinao Amano <akno@live.jp>
2019-10-05 21:46:42 +0900

    シャットダウン判定修正

--- 2019/sh/autohalt.sh
+++ 2019/sh/autohalt.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 rosrun=false
 res0=$(gpio -1 read 40)
+if [ ${res0} = 0 ]; then
+    exit 1
+fi
 res=1
-while [ ${res} = ${res0} ]
+while [ ${res0} = ${res} ]
 do
     sleep 2s
     res=$(gpio -1 read 40)
