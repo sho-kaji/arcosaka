8c33dfc9d7b28047e51e48a80e80b0db5cefdb63
osakarp <arcosaka@outlook.jp>
2019-10-15 11:25:27 +0900

    camera画像が映るようにしました。
    1.mjpg-streamerをinstallしてください。
    手順
    $ git clone https://github.com/jacksonliam/mjpg-streamer.git
    $ cd mjpg-streamer/mjpg-streamer-experimental
    $ make
    $ sudo make install
    
    2.camera.shに記載のpathをinstallパスに適宜書き換えてください。
      *rootの下にtoolフォルダを作成してinstallした環境を想定したpathになっています。
    
    3.camera.shの /dev/videoX のX部分を適宜変更してください。
    　*2眼カメラを2つつないだら、0/1 と 2/3　の組み合わせになるかと予想して
    　 設定しています。
    
    4.camera.shを起動してから
      roslaunch arc2019 client.launch　を起動して
    　debug画面にアクセスすれば、カメラ画像がうつるはずです。。

---
+++ 2019/arc_ws/src/arc2019/www/camera.js
@@ -0,0 +1,7 @@
+document.write('<div class="camera1">\
+             <img src="http://osakarp.local:8494/?action=stream">\
+         </div>\
+         <div class="camera2">\
+             <img src="http://osakarp.local:8495/?action=stream">\
+         </div>\
+');
---
+++ 2019/sh/camera.sh
@@ -0,0 +1,3 @@
+path=~/tool/mjpg-streamer/mjpg-streamer-experimental/
+mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/video0 -y" -o "$path/output_http.so -w ./www -p 8494"
+mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/video1 -y" -o "$path/output_http.so -w ./www -p 8495"
