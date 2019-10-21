6ede064a449b90df62710f093ac9fadd44a77049
a amano <akno@live.jp>
2019-10-21 10:42:36 +0900

    camera.shを修正(未確認)

--- 2019/sh/camera.sh
+++ 2019/sh/camera.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd /dev/
+vdo=$(ls video*)
 path=/usr/local/lib/mjpg-streamer/
-mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/video0 -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p 8494"
-mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/video1 -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p 8495"
+
+for i in ${vdo[@]};do
+    port=${i#video}
+    port=$((10#$port + 8494))
+    mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/$i -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p $port";
+done
