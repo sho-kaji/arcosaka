d54e1bec20670ee75c5bb04db8190b2c7528c807
a amano <akno@live.jp>
2019-10-21 10:46:07 +0900

    camera.sh 少し修正

--- 2019/sh/camera.sh
+++ 2019/sh/camera.sh
@@ -6,5 +6,5 @@ path=/usr/local/lib/mjpg-streamer/
 for i in ${vdo[@]};do
     port=${i#video}
     port=$((10#$port + 8494))
-    mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/$i -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p $port";
+    mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/$i -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p $port" &
 done
