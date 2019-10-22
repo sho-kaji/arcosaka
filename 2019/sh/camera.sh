a458c7a09e240471749c4e447eeb8aef22d5aa65
Akinao Amano <akno@live.jp>
2019-10-22 19:03:44 +0900

    camera.sh 解像度変更

--- 2019/sh/camera.sh
+++ 2019/sh/camera.sh
@@ -6,6 +6,6 @@ path=/usr/local/lib/mjpg-streamer/
 for i in ${vdo[@]};do
     port=${i#video}
     port=$((10#$port + 8494))
-    mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/$i -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p $port" &
+    mjpg_streamer -i "$path/input_uvc.so -f 10 -r 160x120 -d /dev/$i -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p $port" &
     sleep 5s
 done
