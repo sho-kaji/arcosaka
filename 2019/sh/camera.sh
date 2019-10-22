e3c9d25f7260f731f249bb82ef74086c0943bd8d
Akinao Amano <akno@live.jp>
2019-10-22 18:53:38 +0900

    mjpg_streamerの起動タイミングを変更

--- 2019/sh/camera.sh
+++ 2019/sh/camera.sh
@@ -7,4 +7,5 @@ for i in ${vdo[@]};do
     port=${i#video}
     port=$((10#$port + 8494))
     mjpg_streamer -i "$path/input_uvc.so -f 10 -r 640x360 -d /dev/$i -y" -o "$path/output_http.so -w /usr/local/share/mjpg-streamer/www -p $port" &
+    sleep 5s
 done
