47ba51d2598c6735ab82aa0127ae9f777050fe22
Sho Yamamoto <shopokerface@gmail.com>
2020-12-04 21:25:27 +0900

    アームモジュール追加

---
+++ 2020/arc_ws/src/arc2020/scripts/arm/arm.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# license removed for brevity
+
+# pythonでROSのソフトウェアを記述するときにimportするモジュール
+import rospy
+
+# gpio制御用
+import pigpio
+import time
+
+# モーター制御用
+import mortor
+
+# 自分で定義したmessageファイルから生成されたモジュール
+from arc2020.msg import brain
+from arc2020.msg import arm
+
+# 定数などの定義ファイルimport
+from arm_consts import *
+
+CYCLES = 60 
+
+class Arm(object):
+    """
+    全体の動作を制御するクラス
+    """
+#--------------------
+# コンストラクタ
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        self.cyclecount = 0
+        # 受信作成
+        self.sub_brain  = rospy.Subscriber('client2', brain, self.clientCallback, queue_size=1)
+        # 送信作成
+        self.pub_arm = rospy.Publisher('arm', arm, queue_size=100)
+        # messageのインスタンスを作る
+        self.msg_arm = arm()
+        # 送信メッセージ初期化
+        self.clearMsg()
+        
+        # メッセージ受信用変数
+        #self.drill_req = 0  # 受信値B
+        
+        # MortorClass
+        self.stmc = mortor.StepMortorClass(DEBUG_ARM, (PORT_HANDV_A, PORT_HANDV_B), (LIM_HANDV_MIN, LIM_HANDV_MAX))
+        self.svmc = mortor.ServoMortorClass(DEBUG_ARM)
+#--------------------
+
+
+# 送信メッセージの初期化
+    def clearMsg(self):
+        """
+        publishするメッセージのクリア
+        """
+        #for all
+        self.msg_arm.armdrillstatus = 0
+#--------------------
+
+
+# アーム原点復帰
+    def posinit(self):
+        """
+        位置初期化
+        """
+        self.stmc.move_posinit_step()
+#--------------------
+
+
+# アーム動作
+    def move_arm(self):
+        """
+        y軸移動⇒z軸下降⇒z軸上昇
+        """
+        
+        # y軸移動
+        handy = brain_msg.drill_width_value
+        self.stmc.move_step(handy)  # y軸移動
+        
+        
+        # z軸移動
+        self.svmc.move_servo(CHANNEL_HAND, RELEASE_HAND) # z軸上昇
+        time.sleep(2)
+        self.svmc.move_servo(CHANNEL_HAND, CATCH_HAND) # z軸降下
+        time.sleep(2)
+        self.svmc.move_servo(CHANNEL_HAND, RELEASE_HAND) # z軸上昇
+        time.sleep(2)
+#--------------------
+
+
+# 受信コールバック
+    def clientCallback(self, brain_msg):
+        """
+        クライアントの受信コールバック
+        """
+        #print(brain_msg.drill_req)
+        #print(brain_msg.retorg_req)
+        
+        # メッセージ受信
+        
+        
+        if brain_msg.retorg_req :
+            self.msg_arm.armdrillstatus = 2 # アーム原点復帰中
+            self.posinit()
+            self.msg_arm.armdrillstatus = 0 # アーム停止中
+        
+        
+        if brain_msg.drill_req :
+            
+            # 送信用メッセージ更新
+            self.msg_arm.armdrillstatus = 1 # アーム動作中
+            
+            #print(brain_msg.drill_width_value)
+            #print(brain_msg.drill_height_value)
+            
+            # アーム作動
+            
+            self.move_arm()
+            
+            # 送信用メッセージ更新
+            self.msg_arm.armdrillstatus = 0 # アーム停止中
+        else :
+            self.svmc.move_servo(CHANNEL_HAND, RELEASE_HAND)
+#--------------------
+
+
+# メイン関数
+    def main(self):
+        # メッセージを発行する
+        self.pub_arm.publish(self.msg_arm)
+#--------------------
+
+
+def arm_py():
+    # 初期化宣言 : このソフトウェアは"arm_py_node"という名前
+    rospy.init_node('arm_py_node', anonymous=True)
+    # インスタンスの作成 
+    arm = Arm()
+    # 処理周期の設定
+    r = rospy.Rate(CYCLES)
+
+    print("[arm] start")
+    # ctl +　Cで終了しない限りwhileループで処理し続ける
+    while not rospy.is_shutdown():
+        # メイン処理
+        arm.main()
+        #
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        arm_py()
+
+    except rospy.ROSInterruptException:
+        print("[arm] end")
---
+++ 2020/arc_ws/src/arc2020/scripts/arm/arm_consts.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+arm.py 定数ファイル
+"""
+
+from enum import Enum,IntEnum
+
+import pigpio
+
+# defined const
+DEBUG_HAND = False  # ハンドデバッグモードフラグ
+DEBUG_ARM = False  # アームデバッグモードフラグ
+DEBUG_BODY = False  # ボディデバッグモードフラグ
+
+CHANNEL_HAND = 0  # ハンドモーターポート番号
+
+LIM_HANDV_MAX = 3500  # ハンド垂直モーター最大値
+LIM_HANDV_MIN = 0  # ハンド垂直モーター最小値
+
+PORT_HANDV_A = 11  # ハンド垂直モーターAポート番号
+PORT_HANDV_B = 8  # ハンド垂直モーターBポート番号
+PORT_PWOFFSW = 21  # シャットダウンSWポート番号
+
+CATCH_HAND = 100  # ハンドモーター_掴む
+RELEASE_HAND = 0  # ハンドモーター_離す
+
+
+# (.+)\t(.+)\t(.+) $1 = $2 # $3
