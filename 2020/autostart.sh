de97d39c32b4c17a8316631f2195e4909fcd92f2
Sho Yamamoto <shopokerface@gmail.com>
2021-04-03 13:58:49 +0900

    自動スタート用シェル追加

---
+++ 2020/autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#echo "doker container name ?"
+#read containername
+
+#echo "launch file name ?"
+#read launchname
+
+#sudo docker start "$containername"
+
+#sudo docker exec -it "$containername" /bin/bash -c "catkin_make && catkin_make install"
+
+# sudo docker exec -it "$containername" /bin/bash -c "source devel/setup.sh && roslaunch "$containername" "$launchname".launch"
+
+
+sudo docker start debugcont
+
+sudo docker exec -it ledlit /bin/bash -c "source devel/setup.sh && roslaunch arc2020 main.launch"
